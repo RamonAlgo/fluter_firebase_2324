ce73b7d48c0406d002f2ea6261317ae49126afa5
Ramon <ramonandres@ceroca.cat>
2024-03-06 19:45:10 +0100

    Login funcionan

---
+++ lib/auth/servei_auth.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ServeiAuth {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//Fer Login
+
+  Future<UserCredential> loginAmbEmailIPassword(String email, password) async {
+    print(password);
+
+    try{ 
+      
+      UserCredential creadencialUsuari = await _auth.signInWithEmailAndPassword(
+        email: email, 
+        password: password
+        );
+
+        return creadencialUsuari;
+   
+  } on FirebaseAuthException catch (e){
+
+    throw Exception(e);
+  }
+
+//Fer Registre
+
+//Fer logout
+  }
+  }
